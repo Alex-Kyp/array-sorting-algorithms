9cb74627801637ad9f8ec2158c9771f92f15236b
Alex-Kyp <alex.midi.h@gmail.com>
2021-11-15 17:20:13 +0200

    Modified bubble-sort.c

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,3 +1,16 @@
 #Bubble-sort
 #implemantare
 #nici acum nu am implementat ^-^
+
+#include <stdio.h>
+ 
+static void sort(void)
+{
+    // TODO: add bubble sort algorithm here
+}
+ 
+int main()
+{
+    return 0;
+}
+ 
